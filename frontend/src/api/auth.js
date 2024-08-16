ede8c58a876f60c8af73a63fa94cbd6c1b741b26
raj100456 <patelrajkumar579@gmail.com>
2024-08-16 17:39:24 +0530

    profilefyi update redeploy

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
+const API_URL = "https://profilefyiecommerce-backend.onrender.com";
 
 export const register = async (userData) => {
   try {
