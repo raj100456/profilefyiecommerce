693e8ba273462502f726cf64fbb06589cfc1059e
raj100456 <patelrajkumar579@gmail.com>
2024-08-17 07:49:06 +0530

    profile ecomm vercel production deploy

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://profilefyiecommerce.onrender.com/api/auth";
+const API_URL = "https://raj100456-profilefyi-ecommerce.vercel.app/api/auth";
 
 export const register = async (userData) => {
   try {
